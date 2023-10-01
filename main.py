9b41229d80a839403d64a95813c13d6381d3b281
Владимир Доронин <vladimirdoronin@Vladimirs-MacBook-Pro.local>
2023-10-01 15:30:15 +0200

    now with voice

--- main.py
+++ main.py
@@ -1,11 +1,48 @@
 import openai
 import wx
+from wx.lib.buttons import GenButton, GenBitmapTextButton
+import speech_recognition as sr
+from google.cloud import texttospeech
+import os
+
 
 # Ваш секретный API-ключ от OpenAI (важно не публиковать его и не делиться)
-openai.api_key = "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"
+openai.api_key = "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"
 
 # Название файла, в котором будет храниться история разговора
 CONVERSATION_FILE = "conversation_history.txt"
+
+# Для использования вашего API-ключа, вы должны установить его в переменную окружения:
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/vladimirdoronin/NewEd/NewEd.json"
+
+def play_audio(filename):
+    """
+    Воспроизведение аудиофайла.
+    """
+    os.system(f"afplay {filename}")  
+
+def synthesize_speech(text, output_filename='output.mp3'):
+    # Инициализация клиента
+    client = texttospeech.TextToSpeechClient()
+
+    # Настройка запроса
+    input_text = texttospeech.SynthesisInput(text=text)
+    voice = texttospeech.VoiceSelectionParams(
+        language_code="en-US",
+        ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
+    )
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.MP3
+    )
+
+    # Запрос к API для получения аудио
+    response = client.synthesize_speech(
+        input=input_text, voice=voice, audio_config=audio_config
+    )
+
+    # Сохранение аудио в файл
+    with open(output_filename, "wb") as out:
+        out.write(response.audio_content)
 
 # Функция для чтения предыдущих бесед из файла
 def get_previous_conversations():
@@ -132,7 +169,6 @@ class ChatbotApp(wx.Frame):
     def OnRightUp(self, evt):
         self.Close()
 
-    # Инициализация пользовательского интерфейса
     def init_ui(self):
         # Создание панели на основном окне
         pnl = wx.Panel(self)
@@ -148,33 +184,34 @@ class ChatbotApp(wx.Frame):
         vbox.Add((chat_width,353))
         
         # Создание многострочного текстового поля для истории чата с атрибутом READONLY (только для чтения)
-        self.text_history = wx.TextCtrl(pnl, size=(chat_width, chat_height), style=wx.TE_MULTILINE | wx.TE_READONLY)
+        self.text_history = wx.TextCtrl(pnl, size=(chat_width, chat_height), style=wx.TE_MULTILINE | wx.TE_READONLY | wx.BORDER_SUNKEN)
+        self.text_history.SetBackgroundColour("#E2ACAB")
         
         # Создание однострочного текстового поля для ввода сообщений
-        self.input_field = wx.TextCtrl(pnl, size=(chat_width, -1))
-
-        self.text_history.SetBackgroundColour("#E2ACAB")  
+        self.input_field = wx.TextCtrl(pnl, size=(chat_width, -1), style=wx.BORDER_SUNKEN)
         self.input_field.SetBackgroundColour("#E2A9AF")  
-
         
         # Создание кнопки "Send"
         self.send_button = wx.Button(pnl, label='Send', size=(chat_width, -1))
+        # New voice input button
+        self.voice_button = wx.Button(pnl, label='Voice Input', size=(chat_width, -1))
+        self.voice_button.Bind(wx.EVT_BUTTON, self.voice_input)
+
         
         # Привязка события нажатия на кнопку к функции send_message
         self.send_button.Bind(wx.EVT_BUTTON, self.send_message)
 
         # Добавление поля истории чата, поля ввода и кнопки "Send" к вертикальному контейнеру
-
         vbox.Add(self.text_history)
         vbox.Add(self.input_field)
         vbox.Add(self.send_button)
+        vbox.Add(self.voice_button)
 
         hbox_outer = wx.BoxSizer(wx.HORIZONTAL)
 
         hbox_outer.Add((175, 0), proportion=1)  # Пустое пространство слева
         hbox_outer.Add(vbox, proportion=2, flag=wx.EXPAND)
         hbox_outer.Add((chat_width, 0), proportion=1)  # Пустое пространство справа
-
 
         # Установка вертикального контейнера как основного менеджера компоновки для панели
         pnl.SetSizer(hbox_outer)
@@ -185,7 +222,25 @@ class ChatbotApp(wx.Frame):
         # Центрирование окна на экране
         self.Centre()
 
+    def voice_input(self, event):
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            self.text_history.AppendText("Listening...\n")
+            audio = r.listen(source, timeout=5, phrase_time_limit=10)
 
+
+            try:
+                user_message = r.recognize_google(audio)
+                self.text_history.AppendText(f"You: {user_message}\n")
+                bot_response = ask_gpt(user_message)
+                bot_response = bot_response.replace("ChatGPT: ", "")
+                self.text_history.AppendText(f"LisKas: {bot_response}\n")
+                synthesize_speech(bot_response)
+                play_audio('output.mp3')
+            except sr.UnknownValueError:
+                self.text_history.AppendText("Sorry, I did not understand that.\n")
+            except sr.RequestError:
+                self.text_history.AppendText("Could not request results; please check your internet connection.\n")
 
     # Отправка сообщения и получение ответа от бота
     def send_message(self, event):
